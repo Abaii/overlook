2d5ff12c580450a8f924b085133a27b84a825830
Abaii <abaiedmund@gmail.com>
2020-09-16 13:17:13 +0100

    Initial commit :tada:

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import { ThemeProvider } from "@chakra-ui/core";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <div
+        style={{padding: '0 30px'}}
+      >
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
