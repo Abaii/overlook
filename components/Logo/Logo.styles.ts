1bc557c72e44ea6a9591459e30b0e9a77f6e65a9
Abaii <abaiedmund@gmail.com>
2020-09-22 21:57:20 +0100

    Added conditional rendering of animation

--- components/Logo/Logo.styles.ts
+++ components/Logo/Logo.styles.ts
@@ -1,5 +1,9 @@
+import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 
+interface AnimationProps {
+	animation: boolean;
+}
 export const LogoWrapper = styled.div`
 	display: flex;
 	justify-content: center;
@@ -7,23 +11,27 @@ export const LogoWrapper = styled.div`
 	margin-top: 4px;
 `;
 
-export const Icon = styled.div`
-	height: 30px;
-	width: 30px;
-	transform: rotate(45deg);
-	background: linear-gradient(to right, #b4373d, #b23acb);
+export const Icon = styled.div<AnimationProps>`
+  height: 30px;
+  width: 30px;
+  transform: rotate(45deg);
+  background: linear-gradient(to right, #b4373d, #b23acb);
 
-	animation-name: icon-anim;
-	animation-duration: 1.6s;
-	@keyframes icon-anim {
-		from {
-			transform: rotate(45deg);
-		}
-		to {
-			//405 is a full rotation - half is 225
-			transform: rotate(405deg);
-		}
-	}
+  ${({ animation }) =>
+    animation &&
+    css`
+      animation-name: icon-anim;
+    `}
+  animation-duration: 1.6s;
+  @keyframes icon-anim {
+    from {
+      transform: rotate(45deg);
+    }
+    to {
+      //405 is a full rotation - half is 225
+      transform: rotate(405deg);
+    }
+  }
 `;
 
 export const TextWrapper = styled.div`
@@ -31,24 +39,28 @@ export const TextWrapper = styled.div`
 	margin: 0 0 0 0;
 `;
 
-export const UDTriangle = styled.div`
-	width: 0;
-	height: 0;
-	position: relative;
-	left: -25px;
-	top: 4px;
-	border-left: 10px solid transparent !important;
-	border-right: 10px solid transparent !important;
-	border-top: 10px solid white !important;
+export const UDTriangle = styled.div<AnimationProps>`
+  width: 0;
+  height: 0;
+  position: relative;
+  left: -25px;
+  top: 4px;
+  border-left: 10px solid transparent !important;
+  border-right: 10px solid transparent !important;
+  border-top: 10px solid white !important;
 
-	animation-name: udt-anim;
-	animation-duration: 1.2s;
-	@keyframes udt-anim {
-		from {
-			transform: rotate(0deg);
-		}
-		to {
-			transform: rotate(360deg);
-		}
-	}
+  ${({ animation }) =>
+    animation &&
+    css`
+      animation-name: udt-anim;
+    `}
+  animation-duration: 1.2s;
+  @keyframes udt-anim {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
 `;
--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon, LogoWrapper, TextWrapper, UDTriangle } from './Logo.styles';
 import { Text } from '@chakra-ui/core';
 import Typo from '../Typo/Typo';
@@ -6,10 +6,12 @@ import Typo from '../Typo/Typo';
 interface Props {}
 
 export const Logo = (props: Props) => {
+	const [animation, setAnimation] = useState(false);
+
 	return (
-		<LogoWrapper>
-			<Icon />
-			<UDTriangle />
+		<LogoWrapper onClick={() => setAnimation(true)}>
+			<Icon animation={animation}/>
+			<UDTriangle animation={animation}/>
 
 			<TextWrapper>
 				<Typo p_text='Overlook' style={{ fontFamily: 'News Cycle' }} />
